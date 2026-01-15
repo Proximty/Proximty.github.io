448ff2566dec9ec281b30719deb8727d074f5f0a
proximty <hanna@versluizen.nl>
2026-01-15 09:50:24 +0100

    fixed header and workflow

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { siteConfig } from "../siteConfig";
 import ProjectCard from "../components/ProjectCard";
-import projectData from "../data/projectdata.json";
+import projectData from "../data/projectData.json";
 
 export default function Home() {
   const projects = projectData.projects; 
