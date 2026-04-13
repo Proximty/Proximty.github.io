2ee30019d7fd9f9091a677207ca0faadd1c93ff8
proximty <hanna@versluizen.nl>
2026-04-13 23:48:32 +0200

    info aanpassingen

---
+++ src/siteConfig.js
@@ -0,0 +1,25 @@
+export const siteConfig = {
+  name: "Hanna Versluis",
+  role: "XR Developer",
+  tagline: "Ik bouw interactieve XR-ervaringen die zich natuurlijk en intuïtief aanvoelen.",
+  aboutImage: "https://imgur.com/undefined",
+  cv: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+
+  socials: {
+    email: "hanna@versluizen.nl",
+    github: "https://github.com/Proximty",
+    linkedin: "https://linkedin.com/",
+    itch: "https://proxmity.itch.io/",
+  },
+
+  aboutLong: `
+    Ik ben een XR Developer die best wel veel intresse heeft in nieuwe dingen en nooit houd van stil zitten, 
+    fysieke en intuïtieve ervaringen in VR en AR. voor mij draait het niet alleen om leuke games maken maar ook om de ervaringen en iets anders dan mensen voor een pc te zetten.
+    ik hou ook van simulatie maken om te kijken of iets echt mogelijk is al is dat in de biologie of technologie.
+    ik wil mensen in andere vakken helpen om beter te kunnen leeren,
+  `,
+
+  softSkills: ["Samenwerken met artists", "Feedback geven & ontvangen", "Probleemoplossend denken"],
+
+  hardSkills: ["Unity & XR Interaction Toolkit", "C# Scripting", "Git & Version Control"],
+};
